37795eb3d31d16f0efd61bbec98fd0cdfc63cab6
Javik0 <javico.davila@gmail.com>
2021-07-15 16:00:36 -0500

    ji

---
+++ evaluacion/src/COMPONENTES/elemento.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Elemento extends React.Component{
+    
+    eliminarElemento=()=>{
+        this.props.eliminarElementoPractica1(this.props.elem.id);
+    }
+
+    render(){
+        const {elem}= this.props;
+        return <div> 
+        <button style= {elem.color} onClick={this.eliminarElemento}>
+            {elem.nombre}
+        </button>
+        </div>
+    }
+}
+
+export default Elemento;
